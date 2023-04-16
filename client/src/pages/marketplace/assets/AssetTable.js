f47dbdafa73c5b3048c46582b6ea82580e6bf350
Shuhaib-Ahamed <shuhaibsamadh@gmail.com>
2023-04-16 10:54:26 +0530

    fixed bug

--- client/src/pages/marketplace/assets/AssetTable.js
+++ client/src/pages/marketplace/assets/AssetTable.js
@@ -148,7 +148,12 @@ const AssetTable = ({ refresh, setIsOpen, setRefresh }) => {
                               </NavLink>
                               <button
                                 onClick={() =>
-                                  dispatch(getTransaction(asset?.txID))
+                                  dispatch(
+                                    getTransaction({
+                                      txID: asset?.txID,
+                                      txAssetID: asset?.txAssetID,
+                                    })
+                                  )
                                 }
                                 className=" inline-flex items-center py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                               >
